bcd45344050a4c92d4939b8a2cc023030e18d04f
Deuteu <Deuteu@users.noreply.github.com>
2017-05-16 00:05:46 +0200

    Change logic to premature return.

--- main.rb
+++ main.rb
@@ -9,6 +9,8 @@ require 'uri'
 
 require 'telegramAPI'
 
+puts "App name: #{ENV['HEROKU_APP_NAME']}"
+
 api = TelegramAPI.new(ENV['TG_API_TOKEN'].to_s)
 
 def is_admin?(user_id)
@@ -23,68 +25,51 @@ end
 
 post "/#{ENV['TG_WEBHOOK_TOKEN']}" do
   status 200
+  content_type :json
+  empty_json = {}.to_json
 
   # Get Telegram Data
   request.body.rewind
   body = request.body.read
   data = body.length >= 2 ? JSON.parse(body) : nil
 
-  # Output data on stdout
-  p data
-  # {
-  #   "update_id"=>341826966,
-  #   "message"=>{
-  #     "message_id"=>2,
-  #     "from"=>{
-  #       "id"=>123456789,
-  #       "first_name"=>"Deuteu",
-  #       "last_name"=>"Deuteu",
-  #       "username"=>"Deuteu"
-  #     },
-  #     "chat"=>{
-  #       "id"=>123456789,
-  #       "first_name"=>"Deuteu",
-  #       "last_name"=>"Deuteu",
-  #       "username"=>"Deuteu",
-  #       "type"=>"private"
-  #     },
-  #     "date"=>1494875758,
-  #     "text"=>"test"
-  #   }
-  # }
+  puts "Data received: #{data}"
 
-  if data["message"]
-    message = data["message"]
-    if message["chat"] && message["chat"]["id"]
-      from = message["from"]
-      if is_admin?(from["id"])
-        if message["text"]
-          urls = get_urls(message["text"])
-          if urls.empty?
-            puts "NoUrl - Message with no urls: #{message["text"]}"
-            api.sendMessage(message["chat"]["id"], "No urls.")
-          else
-            urls.each do |url|
-              api.sendMessage(message["chat"]["id"], url)
-            end
-          end
-        else
-          puts "EmptyoMessage - Message with no text: #{message}"
-        end
-      else
-        puts "NotAdmin - Message by not admin user: #{from}"
-        api.sendMessage(message["chat"]["id"], "My mum told me not to talk to stranger.")
-      end
-    else
-      puts "MissingArgs - No chat id for response: #{data}"
-    end
-  else
+  unless data["message"]
     puts "MissingArgs - No message: #{data}"
+    return empty_json
+  end
+
+  message = data["message"]
+  unless message["chat"] && message["chat"]["id"]
+    puts "MissingArgs - No chat id for response: #{data}"
+    return empty_json
+  end
+
+  from = message["from"]
+  unless is_admin?(from["id"])
+    puts "NotAdmin - Message by not admin user: #{from}"
+    api.sendMessage(message["chat"]["id"], "My mum told me not to talk to stranger.")
+    return empty_json
+  end
+
+  unless message["text"]
+    puts "EmptyMessage - Message with no text: #{message}"
+    return empty_json
+  end
+
+  urls = get_urls(message["text"])
+  if urls.empty?
+    puts "NoUrl - Message with no urls: #{message["text"]}"
+    api.sendMessage(message["chat"]["id"], "No urls.")
+  else
+    urls.each do |url|
+      api.sendMessage(message["chat"]["id"], url)
+    end
   end
 
   # Return an empty json, to say "ok" to Telegram
-  content_type :json
-  {}.to_json
+  return {}.to_json
 end
 
 #HEROKU_APP_NAME missing so do it manually
