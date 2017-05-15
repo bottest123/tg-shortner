af4ed147d1a38ac0fa590df96ffe979e453eb6f4
Deuteu <Deuteu@users.noreply.github.com>
2017-05-15 20:47:34 +0200

    Add webhook endpoint and capture telegram data.

--- hello.rb
+++ hello.rb
@@ -1,8 +1,23 @@
 if ENV['RACK_ENV'] != 'production'
 	require 'dotenv/load'
 end
-require 'sinatra'
 
-get '/' do
-  "Hello World! #{ENV['test']}"
+require 'sinatra'
+require 'json'
+
+
+get "/#{ENV['TG_WEBHOOK_TOKEN']}" do
+  status 200
+
+  # Get Telegram Data
+  request.body.rewind
+  body = request.body.read
+  data = body.length >= 2 ? JSON.parse(body) : nil
+
+  # Output data on stdout
+  p data
+
+  # Return an empty json, to say "ok" to Telegram
+  content_type :json
+  {}.to_json
 end
